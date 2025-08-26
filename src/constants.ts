daacedc0bf6011c77a6fb238f115acd56edb581c
dawalberto <albertoinformatica97@gmail.com>
2025-08-26 11:33:44 +0200

    feat: add freelance pages

--- src/constants.ts
+++ src/constants.ts
@@ -14,7 +14,6 @@ export enum CipherNavSections {
 
 export const CIPHER_PROJECT = {
   title: "Rediseño integral de Plataforma de Ciberseguridad ( Cipher )",
-  titleIcon: "fingerprint-simple",
   rol: "Product Designer",
   duration: "2 años",
   typeOfProject: "Rediseño completo de producto digital B2B",
@@ -50,7 +49,6 @@ export enum FasNavSections {
 
 export const FAS_PROJECT = {
   title: "Rediseño Web y Gestión Digital – Federación de Artesanos de Sevilla",
-  titleIcon: "yarn",
   rol: "UX/UI Designer",
   duration: "1 año",
   typeOfProject: "Rediseño web y estrategia digital para la Federación de Artesanos de Sevilla",
@@ -84,7 +82,6 @@ export enum CarrefourNavSections {
 
 export const CARREFOUR_PROJECT = {
   title: "Carrefour Pass - Área Financiera",
-  titleIcon: "credit-card",
   rol: "UX/UI Designer",
   duration: "1 año",
   typeOfProject: "Diseño de experiencia y diseño de interfaz para la parte financiera de Carrefour",
@@ -122,7 +119,6 @@ export enum BancSabadellNavSections {
 
 export const BANC_SABADELL_PROJECT = {
   title: "Solicitar un Préstamo Online - Banc Sabadell",
-  titleIcon: "bank",
   rol: "UX Designer",
   duration: "1 año",
   typeOfProject: "UX Research + creación de flujo de solicitud de préstamos online",
@@ -146,5 +142,142 @@ export const BANC_SABADELL_PROJECT = {
     ],
     [BancSabadellNavSections.SECTION_8, { title: "Conclusiones y futuribles", icon: "check-fat" }],
     [BancSabadellNavSections.SECTION_9, { title: "Aprendizaje personal", icon: "brain" }],
+  ]),
+}
+
+export enum RebuildingCitiesGenderPerspectiveNavSections {
+  SECTION_1 = "challenge",
+  SECTION_2 = "process",
+  SECTION_3 = "information-architecture",
+  SECTION_4 = "wireframes and prototypes",
+  SECTION_5 = "conclusions and learning",
+  SECTION_6 = "futuribles",
+}
+
+export const REBUILDING_CITIES_GENDER_PERSPECTIVE_PROJECT = {
+  title: "Reconstruyendo ciudades con perspectiva de género",
+  rol: "UX Designer",
+  duration: "2 semanas",
+  typeOfProject:
+    "Proyecto de UX Research + Diseño de Servicios Urbanos + Diseño de Experiencia Comunitaria.",
+  tools: ["Figma", "Miro", "Trello", "Google Forms"],
+  navItems: new Map<
+    RebuildingCitiesGenderPerspectiveNavSections,
+    { title: string; icon: IconName }
+  >([
+    [
+      RebuildingCitiesGenderPerspectiveNavSections.SECTION_1,
+      { title: "El reto", icon: "puzzle-piece" },
+    ],
+    [
+      RebuildingCitiesGenderPerspectiveNavSections.SECTION_2,
+      { title: "El proceso", icon: "magnifying-glass" },
+    ],
+    [
+      RebuildingCitiesGenderPerspectiveNavSections.SECTION_3,
+      { title: "Arquitectura de la información", icon: "chart-polar" },
+    ],
+    [
+      RebuildingCitiesGenderPerspectiveNavSections.SECTION_4,
+      { title: "Wireframes y prototipo", icon: "pencil-line" },
+    ],
+    [
+      RebuildingCitiesGenderPerspectiveNavSections.SECTION_5,
+      { title: "Conclusiones y aprendizajes", icon: "check-fat" },
+    ],
+    [
+      RebuildingCitiesGenderPerspectiveNavSections.SECTION_6,
+      { title: "Futuribles", icon: "brain" },
+    ],
+  ]),
+}
+
+export enum InkNavSections {
+  SECTION_1 = "challenge",
+  SECTION_2 = "process",
+  SECTION_3 = "information-architecture",
+  SECTION_4 = "wireframes-and-prototypes",
+  SECTION_5 = "conclusions",
+}
+
+export const INK_PROJECT = {
+  title: "Proyecto INK",
+  rol: "UX Designer",
+  duration: "2 semanas",
+  typeOfProject:
+    "Proyecto de UX Research + Diseño de Servicios Urbanos + Diseño de Experiencia Comunitaria.",
+  tools: ["Figma", "Miro", "Trello", "Google Forms"],
+  navItems: new Map<InkNavSections, { title: string; icon: IconName }>([
+    [InkNavSections.SECTION_1, { title: "El reto", icon: "puzzle-piece" }],
+    [InkNavSections.SECTION_2, { title: "El proceso", icon: "magnifying-glass" }],
+    [InkNavSections.SECTION_3, { title: "Arquitectura de la información", icon: "chart-polar" }],
+    [InkNavSections.SECTION_4, { title: "Wireframes y prototipo", icon: "pencil-line" }],
+    [InkNavSections.SECTION_5, { title: "Conclusiones y futuribles", icon: "check-fat" }],
+  ]),
+}
+
+export enum KingsugiNavSections {
+  SECTION_1 = "context",
+  SECTION_2 = "challenge",
+  SECTION_3 = "sprint-process",
+  SECTION_4 = "design",
+  SECTION_5 = "testing",
+  SECTION_6 = "conclusions",
+  SECTION_7 = "personal-thought",
+}
+
+export const KINGSUGI_PROJECT = {
+  title: "Proyecto de Empatía Animal - Kingsugi",
+  rol: "UX Designer",
+  duration: "5 días",
+  typeOfProject: "Design sprint",
+  tools: [
+    "Figma",
+    "Miro",
+    "Trello",
+    "Google Forms",
+    "Maze",
+    "Google Scholar / Artículos online",
+    "Notion",
+  ],
+  navItems: new Map<KingsugiNavSections, { title: string; icon: IconName }>([
+    [KingsugiNavSections.SECTION_1, { title: "Contexto", icon: "book" }],
+    [KingsugiNavSections.SECTION_2, { title: "El desafío", icon: "puzzle-piece" }],
+    [
+      KingsugiNavSections.SECTION_3,
+      { title: "Proceso de sprint", icon: "hourglass-simple-medium" },
+    ],
+    [KingsugiNavSections.SECTION_4, { title: "El diseño", icon: "pencil-line" }],
+    [KingsugiNavSections.SECTION_5, { title: "Testeo", icon: "check-fat" }],
+    [KingsugiNavSections.SECTION_6, { title: "Conclusiones y futuribles", icon: "brain" }],
+    [KingsugiNavSections.SECTION_7, { title: "Reflexión personal", icon: "magic-wand" }],
+  ]),
+}
+
+export enum AtidNavSections {
+  SECTION_1 = "context",
+  SECTION_2 = "challenge",
+  SECTION_3 = "sprint-process",
+  SECTION_4 = "design",
+  SECTION_5 = "testing",
+  SECTION_6 = "conclusions",
+  SECTION_7 = "personal-thought",
+}
+
+export const ATID_PROJECT = {
+  title: "ATID Website Redesign",
+  rol: "UX Designer",
+  duration: "1 año",
+  typeOfProject:
+    "Proyecto de rediseño de sitio web corporativo B2B con enfoque en productos tecnológicos e industriales.",
+  tools: ["Figma", "Miro", "Trello", "Google Scholar / Artículos online", "Notion", "Google Meet"],
+  navItems: new Map<AtidNavSections, { title: string; icon: IconName }>([
+    [AtidNavSections.SECTION_1, { title: "Contexto", icon: "book" }],
+    [AtidNavSections.SECTION_2, { title: "El desafío", icon: "puzzle-piece" }],
+    [AtidNavSections.SECTION_3, { title: "Proceso de sprint", icon: "hourglass-simple-medium" }],
+    [AtidNavSections.SECTION_4, { title: "El diseño", icon: "pencil-line" }],
+    [AtidNavSections.SECTION_5, { title: "Testeo", icon: "check-fat" }],
+    [AtidNavSections.SECTION_6, { title: "Conclusiones y futuribles", icon: "brain" }],
+    [AtidNavSections.SECTION_7, { title: "Reflexión personal", icon: "magic-wand" }],
   ]),
 }
