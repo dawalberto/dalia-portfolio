d3f164086cf66d3c3b6a8468085f0e5a0a06f100
dawalberto <albertoinformatica97@gmail.com>
2025-08-26 12:29:12 +0200

    feat: sitemap and robots files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
+import sitemap from "@astrojs/sitemap"
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://dalialopezrubio.com/",
+  integrations: [sitemap()],
 })
