b08534dc8fc3a8b2b73b4e909eb3b51806ae5918
dawalberto <albertoinformatica97@gmail.com>
2025-08-26 13:39:11 +0200

    chore: update site base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
 import sitemap from "@astrojs/sitemap"
+import { CONFIG } from "./src/config"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  // site: "https://dalialopezrubio.com/",
-  site: "https://dawalberto.github.io/",
-  base: "/dalia-portfolio/", // ⚠️ añade esto si tu repo no es la raíz del usuario
+  site: CONFIG.site,
+  base: CONFIG.base, // ⚠️ añade esto si tu repo no es la raíz del usuario
   integrations: [sitemap()],
 })
---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const CONFIG = {
+  // site: "https://dalialopezrubio.com/" -> cuando esté el dominio configurado,
+  site: "https://dawalberto.github.io/",
+  base: "/dalia-portfolio", // -> eliminar cuando esté el dominio configurado
+}
